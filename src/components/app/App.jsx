5088e41e8a941d247ac57bd1e84c6f93b4662eed
ryanleviathan <ryan.m.carreras@gmail.com>
2021-02-04 18:23:31 -0800

    added app.js path

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import AllCharacters from '../characters/AllCharacters';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <AllCharacters />
+  );
 }
